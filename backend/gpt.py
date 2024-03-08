700d5efec2403818f54673246562c9805d1225d5
Abhiram Vinjamuri <54580211+ramstar3000@users.noreply.github.com>
2024-03-08 20:04:44 +0000

    GPt requests

---
+++ backend/gpt.py
@@ -0,0 +1,36 @@
+from openai import OpenAI
+
+
+def generate_response_gpt(prompt, key):
+
+
+    client = OpenAI(api_key=key)
+
+    context = ""
+
+
+
+    stream = client.chat.completions.create(
+        model="gpt-4",
+        messages=[{"role": "user", "content": context + prompt}],
+        stream=True,
+    )
+    response = ""
+    for chunk in stream:
+        if chunk.choices[0].delta.content is not None:
+            response += chunk.choices[0].delta.content
+            
+    return response
+
+
+if __name__ == "__main__":
+    
+    file = open("backend/secret.txt", "r")
+
+    key = file.read()
+
+    file.close()
+
+    prompt = "What is the meaning of life?"
+
+    print(generate_response_gpt(prompt, key))
