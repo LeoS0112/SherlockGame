43e10759a9604059f1ef12666f153688083adc78
Deathraider122 <fredhuang122106@gmail.com>
2024-03-09 09:14:58 +0000

    improvements

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,8 +1,82 @@
 from django.shortcuts import render
-from rest_framework import generics
-from .models import NPC
-from .serializers import NPCSerializer
+from rest_framework import generics, status
+from rest_framework.response import Response
+from .models import NPC, Map, Conversations, NPCsInLevel, UserGameItem
+from .serializers import NPCSerializer, MapSerializer, ConversationsSerializer, NPCsInLevelSerializer, UserGameItemSerializer
 
+# NPC CRUD
 class NPCListCreate(generics.ListCreateAPIView):
     queryset = NPC.objects.all()
     serializer_class = NPCSerializer
+
+    def delete(self, request, *args, **kwargs):
+        NPC.objects.all().delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class NPCRetrieveUpdateDestory(generics.RetrieveUpdateDestroyAPIView):
+    queryset = NPC.objects.all()
+    serializer_class = NPCSerializer
+    lookup_field = "pk"
+
+#Map CRUD
+class MapListCreate(generics.ListCreateAPIView):
+    queryset = Map.objects.all()
+    serializer_class = MapSerializer
+
+    def delete(self, request, *args, **kwargs):
+        Map.objects.all().delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class MapRetrieveUpdateDestory(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Map.objects.all()
+    serializer_class = MapSerializer
+    lookup_field = "pk"
+
+# Conversation CRUD
+class ConversationListCreate(generics.ListCreateAPIView):
+    queryset = Conversations.objects.all()
+    serializer_class = ConversationsSerializer
+
+    def delete(self, request, *args, **kwargs):
+        Conversations.objects.all().delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class ConversationsRetrieveUpdateDestory(generics.RetrieveUpdateDestroyAPIView):
+    queryset = Conversations.objects.all()
+    serializer_class = ConversationsSerializer
+    lookup_field = "pk"
+
+# NPCsInLevel CRUD
+    
+class NPCsInLevelListCreate(generics.ListCreateAPIView):
+    queryset = NPCsInLevel.objects.all()
+    serializer_class = NPCsInLevelSerializer
+
+    def delete(self, request, *args, **kwargs):
+        NPCsInLevel.objects.all().delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class NPCsInLevelRetrieveUpdateDestory(generics.RetrieveUpdateDestroyAPIView):
+    queryset = NPCsInLevel.objects.all()
+    serializer_class = NPCsInLevelSerializer
+    lookup_field = "pk"
+
+# UserGameItem CRUD
+class UserGameItemListCreate(generics.ListCreateAPIView):
+    queryset = UserGameItem.objects.all()
+    serializer_class = UserGameItemSerializer
+
+    def delete(self, request, *args, **kwargs):
+        UserGameItem.objects.all().delete()
+        return Response(status=status.HTTP_204_NO_CONTENT)
+
+
+class UserGameItemRetrieveUpdateDestory(generics.RetrieveUpdateDestroyAPIView):
+    queryset = UserGameItem.objects.all()
+    serializer_class = UserGameItemSerializer
+    lookup_field = "pk"
+
--- backend/img_uploader.py
+++ backend/img_uploader.py
@@ -9,16 +9,6 @@ import boto3
 
 client = boto3.client('s3', region_name='eu-north-1')
 
-# media_directory = 'backend/media'  # Use base directory for efficient path handling
-
-# for folder in os.listdir(media_directory):
-#     folder_path = os.path.join(media_directory, folder)
-#     for img in os.listdir(folder_path):
-#         img_path = os.path.join(folder_path, img)
-#         key = f"{folder}/{img}"  # Use relative path to preserve structure
-#         with open(img_path, 'rb') as img_file:
-#             client.upload_fileobj(img_file, 'sherlock-game', key)
-
 end = time.perf_counter()
 print(end - start) 
     
@@ -46,7 +36,7 @@ for folder in os.listdir(media_directory):
             carpet_id += 1
 
         elif folder == "npcs" and img not in list_of_npcs:
-            key = f"{folder}/{img}"
+            key = f"{folder}/{npc_id}"
             list_of_npcs.append(img)
             with open(img_path, 'rb') as img_file:
                 client.upload_fileobj(img_file, 'sherlock-game', key, ExtraArgs={'Metadata': {'id': str(npc_id)}})
